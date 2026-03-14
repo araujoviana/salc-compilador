1e73e3260adcb37fbaf124d2f29441f5a42d1082
Matheus Araujo <matheusaraujo1@pm.me>
2026-03-14 18:27:05 -0300

    Lê argumentos da linha de comando

---
+++ opt.c
@@ -0,0 +1,57 @@
+#include <stdbool.h>
+#include <string.h>
+
+/// Flags das opções da linha de comando
+typedef struct {
+  const char *input_file;
+  bool tokens;
+  bool symtab;
+  bool trace;
+} CliOptions;
+
+// Tipos de erro retornado pelo arquivo
+typedef enum {
+  E_OK = 0,
+  E_COUNT,
+  E_PATH,
+  E_FLAG,
+  E_OUT_NULL,
+} ArgErr;
+
+/// Retorna 0 se o parsing foi correto
+ArgErr opts_parse(int argc, char *argv[], CliOptions *out) {
+  // Argumentos não nulos
+  if (out == NULL || argv == NULL)
+    return E_OUT_NULL;
+
+  // Número de argumentos correto
+  if (argc < 2)
+    return E_COUNT;
+
+  // Verifica extensão sal
+  const char *extension = strrchr(argv[1], '.');
+  if (extension == NULL || strcmp(extension, ".sal") != 0) {
+    return E_PATH;
+  }
+
+  // Preenche flags no struct
+  *out = (CliOptions){
+      .input_file = argv[1],
+      .tokens = false,
+      .symtab = false,
+      .trace = false,
+  };
+
+  for (int i = 2; i < argc; i++) {
+    if (strcmp(argv[i], "--tokens") == 0) {
+      out->tokens = true;
+    } else if (strcmp(argv[i], "--symtab") == 0) {
+      out->symtab = true;
+    } else if (strcmp(argv[i], "--trace") == 0) {
+      out->trace = true;
+    } else {
+      return E_FLAG;
+    }
+  }
+  return E_OK;
+}
