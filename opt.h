417f56819c8fb0315fb9ac2bf1a9873350aabac1
Matheus Araujo <matheusaraujo1@pm.me>
2026-03-14 20:38:13 -0300

    Arruma interface do opt

--- opt.h
+++ opt.h
@@ -1,3 +1,8 @@
+#ifndef OPT_H
+#define OPT_H
+
+#include <stdbool.h>
+
 /// Flags das opções da linha de comando
 typedef struct {
   const char *input_file;
@@ -6,5 +11,16 @@ typedef struct {
   bool trace;
 } CliOptions;
 
+// Tipos de erro retornado pelo arquivo
+typedef enum {
+  E_OK = 0,
+  E_COUNT,
+  E_PATH,
+  E_FLAG,
+  E_OUT_NULL,
+} ArgErr;
+
 ArgErr opts_parse(int argc, char *argv[]);
 const CliOptions *opts_get(void);
+
+#endif
