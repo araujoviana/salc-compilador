ddc8d487babb7e245b261e0e4ad35968503edb44
Luis Fernando <luisfernando20140@outlook.com>
2026-03-22 03:39:55 -0300

    feat: Inicia implementação do parse_cmd e corrige parse_glob para usar token ao invés de tk

--- parser.c
+++ parser.c
@@ -1,12 +1,15 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "lex.h"
+#include "diag.h"
 
 static Token token;
 static FILE *src;
 static int line_cnt = 1;
 
 static void advance(void) { token = lex_next(src, &line_cnt); }
+static void parse_cmd(void);
 
 static bool accept(Category c) {
   if (token.category == c) {
@@ -21,6 +24,12 @@ static void expect(Category c, const char *expected) {
     diag_error((char *)expected, token.lexema, token.line);
     exit(EXIT_FAILURE);
   }
+}
+
+static bool starts_cmd(Category c) {
+  return c == sPRINT  ||  c == sSCAN  ||  c == sIF      ||
+         c == sMATCH  ||  c == sFOR   ||  c == sLOOP    || 
+         c == sRETURN ||  c == sSTART ||  c == sIDENTIF;       
 }
 
 static void parse_bco(void) {
@@ -57,7 +66,7 @@ static void parse_ini(void) {
 static void parse_glob(void) {
   expect(sGLOBALS, "globals");
   parse_decls(); // Pelo menos 1 decls
-  while (tk.category == sIDENTIF) {
+  while (token.category == sIDENTIF) {
     parse_decls();
   }
 }
@@ -161,5 +170,32 @@ static void parse_param(void) {
     parse_id();
     expect(sDOIS_PTOS, ":");
     parse_tpo();
+  }
+}
+
+static void parse_cmd(void) {
+  switch (token.category)
+  {
+    case sPRINT:   /* TODO: parse_out() */   break;
+    case sSCAN:    /* TODO: parse_inp() */   break;
+    case sIF:      parse_if();               break;
+    case sMATCH:   /* TODO: parse_mat() */   break;
+    case sFOR:     /* TODO: parse_fr() */    break;
+    case sLOOP:    /* TODO: parse_wh_ou_rpt() */ break;
+    case sRETURN:  /* TODO: parse_ret() */   break;
+    case sSTART:   parse_bco();              break;
+    case sIDENTIF: {
+      Token id = token;  // salva o identificador
+      advance();
+      if (token.category == sATRIB || token.category == sABRE_COLCH)
+        /* TODO: parse_atr(id) */;
+      else if (token.category == sABRE_PARENT)
+        /* TODO: parse_call(id) */;
+      break;
+    }
+  default:
+    diag_error("comando", token.lexema, token.line);
+    exit(EXIT_FAILURE);
+    break;
   }
 }
